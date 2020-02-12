5fe79ba046a807469370209c6c4a1d693ebd8bd1
William Philippe <wplb@ic.ufal.br>
2020-02-12 12:45:07 -0300

    Dijkstra done

---
+++ dijkstra/README.md
@@ -0,0 +1,30 @@
+Para compilar o código:
+
+Na pasta do arquivo, run:
+
+> g++ dijkstra.cpp
+
+Para executar com a entrada:
+No Windows:
+
+> .\a.exe < in.txt
+> No Linux:
+> ./a.out < in.txt
+
+Sobre a entrada:
+5 6
+1 2 4
+1 3 3
+4 3 6
+4 5 2
+2 4 1
+3 5 5
+
+Primeira linha:
+Número de vértices N (1-N) e número de arestas M
+Cada M linha:
+A B C
+Vértice A para B e B para A, C = custo.
+
+Resultado esperado:
+7
--- dijkstra/dijkstra.cpp
+++ dijkstra/dijkstra.cpp
@@ -1,7 +1,84 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+typedef pair<int, int> par;
+
+int distancia[15000];
+int processado[15000];
+
+vector<par> vizinhos[15000];
+
+void bfs(int x)
+{
+
+  distancia[x] = 0;
+
+  priority_queue<par, vector<par>, greater<par>> fila;
+  fila.push(par(distancia[x], x));
+
+  while (true)
+  {
+
+    int daVez = -1;
+
+    while (!fila.empty())
+    {
+
+      int atual = fila.top().second;
+      fila.pop();
+
+      if (!processado[atual])
+      {
+        daVez = atual;
+        break;
+      }
+    }
+
+    if (daVez == -1)
+      return;
+
+    for (int i = 0; i < (int)vizinhos[daVez].size(); i++)
+    {
+
+      int dist = vizinhos[daVez][i].first;
+      int atual = vizinhos[daVez][i].second;
+
+      if (distancia[atual] > distancia[daVez] + dist)
+      {
+
+        distancia[atual] = distancia[daVez] + dist;
+        fila.push(par(distancia[atual], atual));
+      }
+    }
+  }
+}
 
 int main()
 {
+
+  int t = 1;
+
+  int n, m, u, v, d;
+  cin >> n >> m;
+
+  for (int i = 0; i <= n + 5; i++)
+  {
+    distancia[i] = INT_MAX;
+    processado[i] = 0;
+  }
+
+  for (int i = 0; i < m; i++)
+  {
+    cin >> u >> v >> d;
+
+    vizinhos[u].push_back(par(d, v));
+    vizinhos[v].push_back(par(d, u));
+  }
+
+  bfs(1);
+
+  cout << distancia[n] << endl;
+
   return 0;
 }
